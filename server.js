6431d3c4dc10a6ad1d1f790cb797c7a8ea45ef4c
Rachit123kumar <hellobittukumar12@gmail.com>
2024-10-16 19:59:48 +0530

    added data.json

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express=require("express")
+const fs=require("fs")
 
 const app=express();
 
 app.use('/',(req,res)=>{
     res.send("Hello my name is Bittu kumar");
+})
+
+app.use("/product",(req,res)=>{
+    fs.readFileSync('./dev-data/data.json',"utf-8",(err,data)=>{
+        res.send(data);
+    })
+
 })
 
 app.listen(3000)
